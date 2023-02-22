f1657f1831f35cd2c6caca3407c36e7493964523
unix9 <daka1g21.soton.ac.uk>
2023-02-22 11:28:47 +0000

    commit

---
+++ src/main/java/com/unix9/ticket/ticket/TicketApplication.java
@@ -0,0 +1,13 @@
+package com.unix9.ticket.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketApplication.class, args);
+	}
+
+}
